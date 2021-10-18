25689bb666dca21a3609ec35440bbda420605c33
manu-grande-lope <manugrande@gmail.com>
2021-10-18 18:09:58 +0200

    POO-prácticas-18-10-ejercicios-1-a-3

--- students/manu-lope/EJERCICIOS JS/ejercicios-clases-JS/ejercicio-3.js
+++ students/manu-lope/EJERCICIOS JS/ejercicios-clases-JS/ejercicio-3.js
@@ -14,10 +14,29 @@ class Menu {
     }
 
     showPrice() {
-        let cost = document.createElement('p');
+        let cost = document.createElement('h2');
         cost.textContent = (`El precio de este menú es ${this.price}`);
         document.body.appendChild(cost);
     }
+
+// 6- Implementar la función 'updatePrice(newPrice)' como propiedad de la clase menú. 
+//Esta función debe cambiar el precio del menú, por el que reciba por parámetro(newPrice).
+
+    updatePrice(newPrice) {
+        this.price = newPrice;
+        let cost = document.createElement('h1');
+        cost.textContent = (`El nuevo precio de este menú es ${this.price}`);
+        document.body.appendChild(cost);
+    }
+
+// 7- Implementar la función 'changeDish(kind, newDish)' como propiedad de la clase menú. 
+//Esta función debe cambiar el plato que se le ordene (primero, segundo, bebida o 
+//postre <- decisión libre el determinar la forma de controlarlo) 
+//por el nuevo plato introducido por parámetro (newDish).
+
+    // changeDish(kind, newDish) {
+        
+    // }
 
 }
 
@@ -46,24 +65,24 @@ class Meatie extends Menu {
 }
 
 // 5- Crear un objeto de cada clase e imprimir por pantalla el precio del menú.
+// 8- Cambiar el precio de todos los menús y mostrar el menú antes y después del cambio
 
 const menuNoMeat = new Veggie('Tofu hummus', 'Fake chicken','Kombucha', 'Vegan Cheesecake', '200€');
 menuNoMeat.showPrice()
+menuNoMeat.updatePrice('30€')
+
+console.log(menuNoMeat)
 
 const menuLowCal = new lowCal('Air', 'Sliced cucumber','Water', 'Pure stevia', '2€');
 menuLowCal.showPrice()
+menuNoMeat.updatePrice('3€')
+console.log(menuLowCal)
 
 const menuAllMeat = new Meatie('raw cow meat', 'raw goat meat', 'raw morcilla', 'filete empanado en azucar', '1€' );
 menuAllMeat.showPrice()
+menuNoMeat.updatePrice('5€')
 
+console.log(menuAllMeat)
 
-// 6- Implementar la función 'updatePrice(newPrice)' como propiedad de la clase menú. 
-//Esta función debe cambiar el precio del menú, por el que reciba por parámetro(newPrice).
-
-// 7- Implementar la función 'changeDish(kind, newDish)' como propiedad de la clase menú. 
-//Esta función debe cambiar el plato que se le ordene (primero, segundo, bebida o 
-//postre <- decisión libre el determinar la forma de controlarlo) por el nuevo plato introducido por parámetro (newDish).
-
-// 8- Cambiar el precio de todos los menús y mostrar el menú antes y después del cambio
 
 // 9- Cambiar un plato diferente de cada menú y mostrar por pantalla el objeto antes del cambio y después del cambio.
