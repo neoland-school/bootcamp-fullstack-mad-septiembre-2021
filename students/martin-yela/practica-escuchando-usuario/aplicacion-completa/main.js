5a72d01ccb182f7c0e13e56e9ea2247e9b3b6b39
MartinYela <martinyela@hotmail.com>
2021-10-18 09:18:30 +0200

    aplicacion completa casi completa

--- students/martin-yela/practica-escuchando-usuario/aplicacion-completa/main.js
+++ students/martin-yela/practica-escuchando-usuario/aplicacion-completa/main.js
@@ -5,34 +5,68 @@
 //  - Me debe mantener el estado de mi lista de tareas aun cuando apague el ordenador y lo vuelva a encender
 //  El diseño es libre. Si no se os ocurre nada, podéis coger alguna imagen de ejemplo de internet.
 
-
 let form = document.querySelector('.formulario');
-let datoTitle = document.querySelector('.titulo');
-let datoDescription = document.querySelector('.description');
 let listTar = document.querySelector('.tarea_lista');
-let listTar2 = document.querySelector('.descripcion');
-let listTar3 = document.querySelector('.simbolo');
-let contador = 0;
+let url = 'https://img2.freepng.es/20180413/lpq/kisspng-ellipsis-computer-icons-word-punctuation-pause-5ad094373a6815.5818075015236188712392.jpg';
+let contador =0;
+
+
+if (isNaN(parseInt(localStorage.getItem('counter')))){
+    contador = 0;
+}else {
+    console.log('no entro');
+    let num = parseInt(localStorage.getItem('counter'));
+    for (let i=1; i<=num; i++){
+        let val1 = 'titulo' + i;
+        let val2 = 'descripcion' +i ;
+        armarHTML(localStorage.getItem(val1), localStorage.getItem(val2));
+    }
+}
+
 
 form.addEventListener('submit', (e) => {
-    contador++;
     e.preventDefault();
-
-    let simb = document.createElement('li');
-    simb.textContent = '0';
-    listTar3.appendChild(simb);
-
-    
-    let titulo = document.createElement('li');
-    titulo.textContent = e.target.title.value;
-    listTar.appendChild(titulo);
-    localStorage.setItem('elTitulo'+JSON.stringify(contador), titulo.textContent);
-
-    let descrip = document.createElement('li');
-    descrip.textContent = e.target.tarea.value;
-    listTar2.appendChild(descrip);
-    localStorage.setItem('laDescripcion'+JSON.stringify(contador), descrip.textContent);
-    document.querySelector('input').value = '';
-    document.querySelector('textarea').value = '';
+    armarHTML(e.target.title.value, e.target.tarea.value);
 });
 
+
+function armarHTML(valor1, valor2) {
+
+    contador++;
+
+    let linea = document.createElement('li');
+    let simb = document.createElement('img');
+    simb.setAttribute('src', url);
+    linea.appendChild(simb);
+    
+    
+    let titulo = document.createElement('span');
+    titulo.textContent = valor1;
+    linea.appendChild(titulo);
+    localStorage.setItem('titulo'+JSON.stringify(contador), titulo.textContent);
+
+    let descrip = document.createElement('span');
+    descrip.textContent = valor2;
+    linea.appendChild(descrip);
+    listTar.appendChild(linea);
+    localStorage.setItem('descripcion'+JSON.stringify(contador), descrip.textContent);
+
+    document.querySelector('input').value = '';
+    document.querySelector('textarea').value = '';
+
+    localStorage.setItem('counter', JSON.stringify(contador));
+    
+}
+
+let check = document.querySelectorAll('img');
+for (let i =0;i<check.length;i++){
+    check[i].addEventListener('click', () => {
+    check[i].removeAttribute('src');
+    check[i].setAttribute('src', 'https://img2.freepng.es/20181123/qrc/kisspng-check-mark-computer-icons-image-desktop-wallpaper-5bf86a9ce733d9.927478521543006876947.jpg');
+    
+})
+}
+
+
+// ENLACE DE CHECK
+// https://img2.freepng.es/20181123/qrc/kisspng-check-mark-computer-icons-image-desktop-wallpaper-5bf86a9ce733d9.927478521543006876947.jpg
