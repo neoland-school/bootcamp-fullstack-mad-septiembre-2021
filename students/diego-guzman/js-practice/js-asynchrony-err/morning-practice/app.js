eaede875b2d52dfb4a9c5094593ae1e227dcf6ef
Diego Guzmán Medrano <guzmanmedrano.diego@gmail.com>
2021-10-20 18:06:09 +0200

    exercises added

--- students/diego-guzman/js-practice/js-asynchrony-err/morning-practice/app.js
+++ students/diego-guzman/js-practice/js-asynchrony-err/morning-practice/app.js
@@ -1,24 +1,72 @@
 // 1- Obtener la información de la batería utilizando promesas y pintar en el HTML si está cargando y que porcentaje de batería queda
+//checkear version ALEX 
+function drawAcharging(charging){
+    const isChargingDOM  = document.querySelector('.battery__charging');
+    isChargingDOM.textContent = charging;
+}
 
-const myBattery = navigator.getBattery();
+function drawBatteryLevel(batteryLevel){
+    const percentageDOM = document.querySelector('.battery__percentage');
+    percentageDOM.textContent = `${batteryLevel*100}`;
+}
 
-let isCharging = false;
-let batteryPercent = '';
+navigator.getBattery().then((batteryManager)=>{
+    
+    drawAcharging(batteryManager.charging);
+    drawBatteryLevel(batteryManager.level);
 
-navigator.getBattery().then(function (battery) {
-    const isChargingDOM = document.createElement('p');
-    setInterval(function () {
-        isCharging = battery.charging;
-        isChargingDOM.innerHTML = isCharging;
-        document.body.appendChild(isChargingDOM);
-    }, 500)
+    //escuchamos el evento de cambio de carga cuando enchufamos y desenchufamos el adaptador
+    batteryManager.addEventListener('chargingchange', ()=>{
+        drawAcharging(batteryManager.charging);
+    });
+    batteryManager.addEventListener('levelchange', ()=>{
+        drawBatteryLevel(batteryManager.level);
+    });
 })
 
-navigator.getBattery().then(function (percentage) {
-    const percentageDOM = document.createElement('p');
-    setInterval(function () {
-        batteryPercent = `${percentage.level}`*100 + '%';
-        percentageDOM.innerHTML = batteryPercent;
-        document.body.appendChild(percentageDOM);
-    }, 500)
-})
+
+//Version async/await
+
+async function drawBatteryStatus(){
+    const batteryManager = await navigator.getBattery();
+    drawAcharging(batteryManager.charging);
+    drawBatteryLevel(batteryManager.level);
+}
+
+drawBatteryStatus();
+
+
+//Version ALEX
+
+// function drawCharging(charging) {
+//     const chargingDOM = document.querySelector("[data-battery-charging]");
+//     chargingDOM.textContent = charging;
+//   }
+
+//   function drawBatteryLevel(batteryLevel){
+//     const percentDOM = document.querySelector("[data-battery-percent]");
+//     percentDOM.textContent = `${batteryLevel * 100}%`;
+//   }
+
+//   navigator.getBattery().then((batteryManager) => {
+//     drawBatteryLevel(batteryManager.level);
+//     drawCharging(batteryManager.charging);
+
+//     // escuchamos el evento de cambio de carga (cuando enchufamos y desenchufamos el adaptador)
+//     batteryManager.addEventListener("chargingchange", () => {
+//       drawCharging(batteryManager.charging);
+//     });
+//     // escuchamos cuando cambia el porcentaje de batería
+//     batteryManager.addEventListener("levelchange", () => {
+//         drawBatteryLevel(batteryManager.level);
+//     });
+//   });
+
+//   // versión async/await
+//   async function drawBatteryStatus(){
+//     const batteryManager = await navigator.getBattery();
+//     drawBatteryLevel(batteryManager.level);
+//     drawCharging(batteryManager.charging);
+//   }
+
+//   drawBatteryStatus();
