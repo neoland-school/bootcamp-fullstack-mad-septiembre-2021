0f02f75abf0e6db7825d8c45f9d2aab74ca67057
Axel Brigo <axelbrigo1993@gmail.com>
2021-10-29 08:54:13 +0200

    Ejercicios del dia 29/10

---
+++ students/axel-brigo/tarea-javascript/EJERCICIOS-REACT-28-10/EJERCICIO-1/CityMadrid.jsx
@@ -0,0 +1,12 @@
+class CityMadrid extends React.Component {
+
+
+    render() {
+        return (
+            <div className="border">
+                <h1>Ciudad: Madrid.</h1>
+                <p>Cantidad de habitantes: 3,207,247 </p>
+            </div>
+        )
+    }
+}
