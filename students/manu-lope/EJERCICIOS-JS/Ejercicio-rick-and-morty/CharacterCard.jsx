dee555fd0c2e5a6ef03ef327f99979e0f58d9d0c
manu-grande-lope <manugrande@gmail.com>
2021-11-03 14:46:38 +0100

    cambios staggeados para cambiar rama

--- students/manu-lope/EJERCICIOS-JS/Ejercicio-rick-and-morty/CharacterCard.jsx
+++ students/manu-lope/EJERCICIOS-JS/Ejercicio-rick-and-morty/CharacterCard.jsx
@@ -1,15 +1,24 @@
-class CharacterCard extends React.Component{
-    constructor(){
-        super()
+class CharacterCard extends React.Component {
+    constructor(props) {
+        super(props)
     }
 
-    render(){
+
+    render() {
         return (
-            <div>
-                <h1>{this.props.name}</h1>
-                <p>{this.props.status}</p>
-                <p>{this.props.location.name}</p>
-                <p>{this.props.episode[0]}</p>
+            <div className="divContainer___style">
+                <img className = "divContainer___style--img"src={this.props.image} alt="imagen personaje" />
+                <div className="divContainer__style--textdiv">
+                    <h1 className="divContainer___style--h1">{this.props.name}</h1>
+                        <div className="div__status">
+                        {this.props.status==="Alive" ? <div className="verde"></div> : <div className="rojo"></div>}
+                        <p>{this.props.status}</p>
+                    </div>
+                    <p className="divContainer___style--p">Last known location:</p>
+                    <p>{this.props.location}</p>
+                    <p className="divContainer___style--p">First seen in:</p>
+                    <p>{this.props.episode}</p>
+                </div>
             </div>
         )
     }
